8eb109afabadc40e98f78c7520370c4603658999
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-12-05 11:50:25 -0800

    repro app

---
+++ Worker.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace UserSecretsInWorkerRepro
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _config;
+
+        public Worker(ILogger<Worker> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation("The secret is: {Secret}", _config["SecretName"]);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
